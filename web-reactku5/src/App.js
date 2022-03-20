4fb12891d89b5b8849db46e7e674d331b6cb3726
Rasyid-deve <renaldirasyed@gmail.com>
2022-03-21 00:43:57 +0700

    Update versi react router dan app.js

--- web-reactku5/src/App.js
+++ web-reactku5/src/App.js
@@ -1,25 +1,75 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// Dalam project ini saya menggunakan react router V5 (5.3.0), agar bisa menggunakan Switch sebab jika react router di install secara base default, 
+// maka Switch sudah di ganti dengan Routes
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <hr />
+
+        {/* <Routes> */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        {/* </Routes> */}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+// export default BasicExample;
+// export default App;
